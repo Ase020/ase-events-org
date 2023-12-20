d227aff8ec9c1d885cbb429c162905072d37b022
felixNyalenda <felixasejunior@gmail.com>
2023-12-20 14:40:01 +0300

    init

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1 className="text-4xl">ase Evently</h1>
+    </main>
+  );
+}
