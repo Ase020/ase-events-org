d3fbf9007eb93f8b5678f95a217329581bb1b0c5
felixNyalenda <felixasejunior@gmail.com>
2023-12-20 15:57:16 +0300

    footer build

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div className="flex-center flex-between wrapper flex flex-col gap-4 p-5 text-center sm:flex-row bg-red-200">
+        <Link href="">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+
+        <p>2023 ase Evently. All Rights reserved.</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
