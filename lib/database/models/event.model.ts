38d2aa74ba3a56ad9c479dff250273247cd0bdf5
felixNyalenda <felixasejunior@gmail.com>
2023-12-21 12:34:58 +0300

    Webhook config & build

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -1,5 +1,4 @@
-import { Schema, model, models } from "mongoose";
-import Document from "next/document";
+import { Document, Schema, model, models } from "mongoose";
 
 export interface IEvent extends Document {
   _id: string;
